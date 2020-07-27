fcd9ad5543d26d12d45a1cd7a20bc44921804362
Raaj52 <mail2raaj52@gmail.com>
2020-07-27 13:16:55 +0530

    davinci: prebuilts: Add GCam GO
    Signed-off-by: Raaj52 <mail2raaj52@gmail.com>

--- device.mk
+++ device.mk
@@ -107,9 +107,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshim_megvii
 
-PRODUCT_PACKAGES += \
-    Snap
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
@@ -218,6 +215,10 @@ PRODUCT_COPY_FILES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.davinci
+
+# Prebuilt Packages
+PRODUCT_PACKAGES += \
+    GCamGo
 
 # Media
 PRODUCT_PACKAGES += \
