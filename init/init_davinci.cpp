fb201b6afadba25f9bc7411254e005ebe8dd173f
Zadli <00vopoj00@gmail.com>
2020-11-25 16:07:30 +0000

    davinci: use fp from Redfin

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -111,7 +111,7 @@ void vendor_load_properties() {
 
     // SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    fingerprint = "google/sunfish/sunfish:11/RP1A.201105.002/6869500:user/release-keys";
+    fingerprint = "google/redfin/redfin:11/RD1A.201105.003.A1/6886512:user/release-keys";
 
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
